05ab29138cc0eec57f1c5f19a4a9a51d9927c478
mccooll <github@no3.ca>
2018-12-24 12:19:08 -0500

    basic binding of journal entry values (lots of work to do; all data mocked, no NEW/DELETE functionality; may need to add new record component

--- src/app/journal-entry/journal-entry.component.ts
+++ src/app/journal-entry/journal-entry.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import JournalEntry from '../model/JournalEntry';
 
 @Component({
   selector: 'app-journal-entry',
@@ -6,10 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./journal-entry.component.less']
 })
 export class JournalEntryComponent implements OnInit {
+  @Input() entry: JournalEntry;
+  @Input() accounts: Account[];
 
-  constructor() { }
+  constructor() {
+
+  }
 
   ngOnInit() {
+  }
+
+  get instantFormatted() {
+  	return this.entry.instant.toISOString().substring(0, 10);
+  }
+
+  set instantFormatted(e) {
+  	let newDate = new Date(e);
+  	this.entry.instant.setTime(newDate.getTime());
   }
 
 }

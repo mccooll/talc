bc4bc665bf979561326a3fda453a0739ddf83770
mccooll <github@no3.ca>
2018-12-27 11:16:06 -0500

    auto support of debit and credit types in record

--- src/app/model/JournalRecord.ts
+++ src/app/model/JournalRecord.ts
@@ -5,4 +5,20 @@ export default class JournalRecord {
   amount: number;
   account: Account;
   journalEntry: JournalEntry;
+
+  get debit() {
+  	return this.amount > 0 ? this.amount: null;
+  }
+
+  set debit(n) {
+  	if(n > 0) this.amount = n;
+  }
+
+  get credit() {
+  	return this.amount < 0 ? -1*this.amount: null;
+  }
+
+  set credit(n) {
+  	if(n > 0) this.amount = -1*n;
+  }
 }

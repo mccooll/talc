461192564867ddd47a4cdb329c3b096f9d875754
mccooll <github@no3.ca>
2018-12-28 13:38:53 -0500

    WIP more crud

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,12 +12,11 @@ import AccountType from './model/AccountType'
 })
 export class AppComponent {
   title = 'TALC';
-  entry = new JournalEntry();
+  entries: JournalEntry[];
   accounts: Account[];
 
   constructor() {
-  	this.entry.instant = new Date();
-  	this.entry.note = "Purchased sporting goods for the community room.";
+  	this.entries = [];
   	let dr = new AccountType();
   	dr.code = false;
   	let cr = new AccountType();
@@ -31,15 +30,31 @@ export class AppComponent {
   	expense.name = "Expense";
   	expense.number = 1;
   	expense.accountType = cr;
+  	this.entries[0] = new JournalEntry();
+  	this.entries[0].instant = new Date();
+  	this.entries[0].note = "Purchased sporting goods for the community room.";
   	let a = new JournalRecord();
   	let b = new JournalRecord();
   	a.amount = 42.33;
   	a.account = cash;
   	b.amount = -42.33;
   	b.account = expense;
-  	this.entry.journalRecords = [];
-  	this.entry.journalRecords.push(a);
-  	this.entry.journalRecords.push(b);
+  	this.entries[0].journalRecords = [];
+  	this.entries[0].journalRecords.push(a);
+  	this.entries[0].journalRecords.push(b);
+  	this.entries[0].journalRecords.push(new JournalRecord());
+  	this.entries[1] = new JournalEntry();
+  	this.entries[1].instant = new Date(new Date().setDate(new Date().getDate()-1));
+  	this.entries[1].note = "Bill for chewing gum.";
+  	let c = new JournalRecord();
+  	let d = new JournalRecord();
+  	c.amount = 4.33;
+  	c.account = cash;
+  	d.amount = -4.33;
+  	d.account = expense;
+  	this.entries[1].journalRecords = [];
+  	this.entries[1].journalRecords.push(c);
+  	this.entries[1].journalRecords.push(d);
   	this.accounts = [cash,expense];
   }
 }

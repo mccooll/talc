3f2a88dbe9bb23b91e681edad7370ae0f904ec7e
mccooll <github@no3.ca>
2019-01-02 15:39:31 -0500

    this is a nice first take at adding an entry

--- src/app/journal/journal.component.ts
+++ src/app/journal/journal.component.ts
@@ -10,10 +10,24 @@ import JournalEntry from '../model/JournalEntry';
 export class JournalComponent implements OnInit {
   @Input() entries: JournalEntry[];
   @Input() accounts: Account[];
+  blankEntry: JournalEntry;
 
-  constructor() { }
+  constructor() {
+  	this.blankEntry = new JournalEntry();
+  }
 
   ngOnInit() {
   }
 
+  addValidBlank():void {
+  	console.log('gotit');
+  	let newEntry = this.blankEntry;
+  	this.entries.push(newEntry);
+  	setTimeout(()=> {
+  	  this.blankEntry = new JournalEntry();
+  	})
+  	//problem here: typing in a number immediately creates a new record and interrupts input focus (maybe we need a debounce delay on this with an observable)
+  	//another problem: it's added to the bottom
+  	return;
+  }
 }

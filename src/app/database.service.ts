735a8d127b748738dcc4b903640bf5487116c34d
Dave <dave@no3.ca>
2020-01-30 22:05:02 -0500

    move db sync to db service and construct entries observable to be more dynamic

--- src/app/database.service.ts
+++ src/app/database.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import PouchDB from 'pouchdb';
+import { bindCallback, Observable} from 'rxjs';
+
 
 @Injectable({
   providedIn: 'root'
@@ -8,6 +10,7 @@ export class DatabaseService {
   
   db:any;
   remotedb:any;
+  syncing:Observable;
 
   constructor() {
   	const user = localStorage.getItem('user') || 'default';
@@ -19,5 +22,11 @@ export class DatabaseService {
 
   	this.db = new PouchDB(localDatabaseName);
     this.remotedb = new PouchDB(`http://${user}:${password}@${serverAddress}:${remoteDatabasePort}/${remoteDatabaseName}`);
+    const sync = this.db.sync(this.remotedb, {
+      live: true,
+      retry: true
+    });
+    const getSyncAsObservable = bindCallback(sync.on);
+    this.syncing = getSyncAsObservable.call(sync, 'change');
   }
 }
--- src/app/journal.service.ts
+++ src/app/journal.service.ts
@@ -11,46 +11,29 @@ import { bindCallback, Observable, of } from 'rxjs';
 export class JournalService {
   entries: JournalEntry[] = [];
   db: any;
-  remotedb: any;
 
   constructor(private accountService: AccountService, private data: DatabaseService) {
   	this.db = this.data.db;
-    this.remotedb = this.data.remotedb;
-    const sync = this.db.sync(this.remotedb, {
-      live: true,
-      retry: true
-    });
-    const getSyncAsObservable = bindCallback(sync.on);
-    getSyncAsObservable.call(sync, 'change').subscribe(x => console.log(x), e => console.error(e));
-    // const result = getSyncAsObservable('change');
-    // result.subscribe(x => console.log(x), e => console.error(e));
-    // .on('change', function (change) {
-    //   console.log(change);
-    //   this.getEntries();
-    // });
   }
 
   getEntries(filter:object): Observable<JournalEntry[]> {
-  	//uh oh - async - we gonna need observables
-  	let accounts = this.accountService.getAccounts();
-  	this.db.allDocs({
-  		include_docs: true,
-      descending: true
-  	}).then((result) => {
-  	  console.log(result.rows);
-  	  result.rows.forEach((row)=> {
-  	  	this.entries.push(new JournalEntry(row.doc,accounts));
-  	  });
-  	  console.log(this.entries);
-  	});
-  	// filter.start
-  	// filter.end
-  	return of(this.entries);
+    let accounts = this.accountService.getAccounts();
+    return new Observable((observer) => {
+      let entries: JournalEntry[] = []; 
+      this.db.allDocs({
+        include_docs: true,
+        descending: true
+      }).then((result) => {
+        entries = [];
+        result.rows.forEach((row)=> {
+          entries.push(new JournalEntry(row.doc,accounts));
+        });
+        observer.next(entries);
+      });
+    });
   }
 
   deleteEntry(j: JournalEntry): JournalEntry[] {
-  	let index = this.entries.indexOf(j);
-  	let deleted: JournalEntry[] = this.entries.splice(index,1);
     this.db.remove(j.getCommittable());
   	return deleted;
   }

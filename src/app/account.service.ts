daf5d7058e5c781642803cb8b7c7432db42d316f
Dave <dave@no3.ca>
2020-02-29 12:23:47 -0500

    WIP

--- src/app/account.service.ts
+++ src/app/account.service.ts
@@ -1,17 +1,30 @@
 import { Injectable } from '@angular/core';
 import Account from './model/Account'
+import { DatabaseService } from './database.service'
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class AccountService {
   accounts: Account[];
+  db: any;
 
-  constructor() {
+  constructor(private data: DatabaseService) {
   	this.accounts = [];
+    this.db = this.data.db;
   }
 
   getAccounts(): Account[] {
     return this.accounts;
+  }
+
+  commitEntry(a: Account): Boolean {
+    this.db.put(a.getCommittable());//.then(function (response) {
+    //   if(response && response.ok && response.rev) {
+    //     a.setRev(response.rev);
+    //   }
+    // });
+    return true;
   }
 }
